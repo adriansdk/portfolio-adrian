56a0d7790fed202411be57466aef7e6c0f552a59
Adrian Visnieski <adrianvisnieski@gmail.com>
2025-04-09 01:27:59 -0400

    Functional next js cloudflare implementation

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: "file-loader?publicPath=/_next/static/files&name=[name].[ext]",
-    });
-    return config;
-  },
-};
-
-export const runtime = "edge";
-
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform();
 }
 

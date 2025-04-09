e354fdddd59f45d1f034720504b67ee323d86194
Adrian Visnieski <adrianvisnieski@gmail.com>
2025-04-08 21:45:01 -0400

    Cloudflare deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.pdf$/,
-      use: 'file-loader?publicPath=/_next/static/files&name=[name].[ext]',
+      use: "file-loader?publicPath=/_next/static/files&name=[name].[ext]",
     });
     return config;
   },
 };
+
+export const runtime = "edge";
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
